bbbe3c9a3845041c6f41ca2ee4df8768d103fe8a
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-02-27 18:55:48 -0500

    Update Client Model

--- Models/Client.cs
+++ Models/Client.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace BackendJPMAnalysis.Models
+﻿namespace BackendJPMAnalysis.Models
 {
-    public partial class Client
+    public partial class Client : BaseEntity
     {
         /// <summary>
-        /// Autoincremental
+        /// Auto-incremental ID
         /// </summary>
-        public int Id { get; set; }
+        public int Id { get; }
         public string? ProductId { get; set; }
         public string? AccountNumber { get; set; }
-        public DateTime? CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
-        public DateTime? DeletedAt { get; set; }
 
         public virtual Account? AccountNumberNavigation { get; set; }
         public virtual Product? Product { get; set; }
