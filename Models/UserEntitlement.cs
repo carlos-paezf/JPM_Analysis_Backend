9e6a03275cdd7622a9ef09e00c9b712537a551f3
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-02-27 19:09:44 -0500

    Update UserEntitlement Model

--- Models/UserEntitlement.cs
+++ Models/UserEntitlement.cs
@@ -1,22 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
+
 
 namespace BackendJPMAnalysis.Models
 {
-    public partial class UserEntitlement
+    public partial class UserEntitlement : BaseEntity
     {
         /// <summary>
-        /// Autoincremental
+        /// Auto-incremental ID
         /// </summary>
-        public int Id { get; set; }
-        public string? AccessId { get; set; }
-        public string? ProductId { get; set; }
-        public string? FunctionId { get; set; }
+        public int Id { get; }
+
+        [Required(ErrorMessage = "La propiedad `accessId` es requerida")]
+        public string AccessId { get; set; } = null!;
+
         public string? FunctionType { get; set; }
+
         public string? AccountNumber { get; set; }
-        public DateTime? CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
-        public DateTime? DeletedAt { get; set; }
+
+        [Required(ErrorMessage = "La propiedad `productId` es requerida")]
+        public string ProductId { get; set; } = null!;
+
+        public string? FunctionId { get; set; }
 
         public virtual CompanyUser? Access { get; set; }
         public virtual Account? AccountNumberNavigation { get; set; }
