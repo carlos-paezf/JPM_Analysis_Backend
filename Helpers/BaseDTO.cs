e1f7b2815b21e151fc38c73cbe7ec7f10d88d9bb
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-03-06 10:21:17 -0500

    Update Base DTO class

--- Helpers/BaseDTO.cs
+++ Helpers/BaseDTO.cs
@@ -1,16 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
-
 namespace BackendJPMAnalysis.Helpers
 {
     public abstract class BaseDTO
     {
-        [Required(ErrorMessage = "La propiedad `createdAt` es requerida")]
         public DateTime CreatedAt { get; set; }
-
-        [Required(ErrorMessage = "La propiedad `updatedAt` es requerida")]
         public DateTime UpdatedAt { get; set; }
-
         public DateTime? DeletedAt { get; set; }
     }
 }
