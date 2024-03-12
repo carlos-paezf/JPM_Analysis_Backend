8b28e92243bffad82b6e7a4d36f39a6c79fadf65
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-03-12 14:17:48 -0500

    Update Product Model

--- Models/Product.cs
+++ Models/Product.cs
@@ -7,10 +7,10 @@ namespace BackendJPMAnalysis.Models
 {
     public partial class ProductModel : BaseModel
     {
+        private string? _productName;
+
         public ProductModel()
         {
-            Id = StringUtil.SnakeCase(ProductName);
-
             Clients = new HashSet<ClientModel>();
             UserEntitlements = new HashSet<UserEntitlementModel>();
         }
@@ -19,10 +19,18 @@ namespace BackendJPMAnalysis.Models
         /// Product name in snake_case
         /// </summary>
         [Key]
-        public string Id { get; } = null!;
+        public string Id { get; private set; } = null!;
 
         [Required(ErrorMessage = "La propiedad `productName` es requerida")]
-        public string ProductName { get; set; } = null!;
+        public string ProductName
+        {
+            get => _productName!;
+            set
+            {
+                _productName = value;
+                Id = StringUtil.SnakeCase(value);
+            }
+        }
 
         public string? SubProduct { get; set; }
 
