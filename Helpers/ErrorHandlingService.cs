21571c82dd08afb36251f464674a1353b46f2fb5
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-03-18 10:49:37 -0500

    Update DOCS and return Status Code 500

--- Helpers/ErrorHandlingService.cs
+++ Helpers/ErrorHandlingService.cs
@@ -15,29 +15,32 @@ namespace BackendJPMAnalysis.Helpers
     {
 #pragma warning disable CS1998
         /// <summary>
-        /// The function `HandleExceptionAsync` handles different types of exceptions by returning
-        /// appropriate HTTP status codes and messages.
+        /// The <c>HandleExceptionAsync</c> method handles different types of exceptions by returning
+        /// appropriate HTTP status codes and messages. It checks the type of exception encountered
+        /// and returns corresponding HTTP status codes along with relevant error messages in JSON format.
+        /// If the exception is a known type, such as <see cref="BadRequestException"/>,
+        /// <see cref="ItemNotFoundException"/>, or <see cref="DuplicateException"/>, it returns specific
+        /// status codes (404 for not found, 409 for conflict, etc.) and custom error messages.
+        /// If the exception is of type <see cref="DbUpdateException"/> and contains an inner
+        /// exception of type <see cref="MySqlException"/> with error number 1062, indicating a duplicate
+        /// entry error in MySQL, it returns a conflict status code (409) with a predefined message
+        /// for duplicate entries.
+        /// If the exception is of an unknown type, it logs the error and returns a generic
+        /// status code 500 (Internal Server Error) along with a generic error message.
         /// </summary>
-        /// <param name="ex">The `HandleExceptionAsync` method is designed to handle different
-        /// types of exceptions and return appropriate responses based on the type of exception
-        /// encountered.</param>
-        /// <param name="logger">ILogger is an interface provided by ASP.NET Core for logging. It
-        /// allows you to log messages from your application code.ILogger provides methods like
-        /// LogInformation, LogError, LogWarning, etc. to log messages with different log levels.ILogger
-        /// is typically injected into classes where logging is needed, such as controllers</param>
-        /// <param name="className">The `className` parameter in the `HandleExceptionAsync` method
-        /// represents the name of the class where the exception occurred. It is used for logging
-        /// purposes to identify the source of the error within the codebase.</param>
-        /// <param name="methodName">The `methodName` parameter in the `HandleExceptionAsync` method
-        /// refers to the name of the method where the exception occurred. It is used for logging
-        /// purposes to identify the specific method that threw the exception.</param>
+        /// <param name="ex">The <paramref name="ex"/> parameter represents the exception that occurred
+        /// and needs to be handled.</param>
+        /// <param name="logger">The <paramref name="logger"/> parameter is an instance of the
+        /// <see cref="ILogger"/> interface used for logging error messages.</param>
+        /// <param name="className">The <paramref name="className"/> parameter represents the name of
+        /// the class where the exception occurred, used for logging purposes.</param>
+        /// <param name="methodName">The <paramref name="methodName"/> parameter represents the name
+        /// of the method where the exception occurred, used for logging purposes.</param>
         /// <returns>
-        /// The method `HandleExceptionAsync` returns an `ActionResult` based on the type of exception
-        /// passed to it. If the exception is of type `ItemNotFoundException`, it returns a
-        /// `NotFoundObjectResult` with a JSON object containing the message, status code 404, and an
-        /// error flag. If the exception is of type `DuplicateException`, it returns a
-        /// `ConflictObjectResult` with similar JSON structure
+        /// An <see cref="ActionResult"/> representing an HTTP response containing the appropriate
+        /// status code and error message based on the type of exception encountered.
         /// </returns>
+
         public async Task<ActionResult> HandleExceptionAsync(Exception ex, ILogger logger, string className, string methodName)
         {
             if (ex is BadRequestException)
@@ -87,7 +90,15 @@ namespace BackendJPMAnalysis.Helpers
             else
             {
                 logger.LogError(ex, $"{className} - {methodName}: Ha ocurrido un error desconocido");
-                return new StatusCodeResult(500);
+                return new ObjectResult(
+                    new
+                    {
+                        message = ex.Message,
+                        status = 500,
+                        error = true
+                    }
+                )
+                { StatusCode = 500 };
             }
         }
 #pragma warning restore CS1998
