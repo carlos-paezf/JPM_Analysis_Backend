6a3a5ee7368dda590429166ac86068cdc8483b22
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-03-18 10:45:16 -0500

    Update ProfileFunction Model

--- Models/ProfileFunction.cs
+++ Models/ProfileFunction.cs
@@ -1,22 +1,33 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
+using BackendJPMAnalysis.Helpers;
 
 
 namespace BackendJPMAnalysis.Models
 {
     public partial class ProfileFunctionModel
     {
+        private string? _functionId;
+
         /// <summary>
-        /// Auto-incremental ID
+        /// ProfileID and FunctionID in snake_case
         /// </summary>
         [Key]
-        public string Id { get; } = null!;
+        public string Id { get; private set; } = null!;
 
         [Required(ErrorMessage = "La propiedad `profileId` es requerida")]
         public string ProfileId { get; set; } = null!;
 
         [Required(ErrorMessage = "La propiedad `functionId` es requerida")]
-        public string FunctionId { get; set; } = null!;
+        public string FunctionId
+        {
+            get => _functionId!;
+            set
+            {
+                _functionId = value;
+                Id ??= StringUtil.SnakeCase(ProfileId + '_' + FunctionId);
+            }
+        }
 
         [JsonIgnore]
         public virtual FunctionModel? Function { get; set; }
