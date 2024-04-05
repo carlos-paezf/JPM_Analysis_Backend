69f1c9724f39694dc94579ab35185a8345f494a5
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-04-05 11:25:50 -0500

    Update Extensión method

--- Helpers/EntityExtensions.cs
+++ Helpers/EntityExtensions.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
 namespace BackendJPMAnalysis.Helpers
 {
     public static class EntityExtensions
     {
-        public static string GetId<T>(this T entity) where T : class
+        /* public static string GetId<T>(this T entity) where T : class
         {
             var properties = typeof(T).GetProperties();
             var idProperty = properties
@@ -14,6 +17,16 @@ namespace BackendJPMAnalysis.Helpers
                 ?? throw new InvalidOperationException($"No se encontró una propiedad Key en la entidad '{typeof(T).Name}'.");
 
             var idValue = idProperty.GetValue(entity);
+            return idValue?.ToString() ?? string.Empty;
+        } */
+
+        public static string GetId<T>(this T entity) where T : class
+        {
+            var keyProperty = typeof(T).GetProperties()
+                .FirstOrDefault(prop => prop.GetCustomAttribute<KeyAttribute>() != null)
+                ?? throw new InvalidOperationException($"No se encontró una clave primaria para la entidad '{typeof(T).Name}'");
+
+            var idValue = keyProperty.GetValue(entity);
             return idValue?.ToString() ?? string.Empty;
         }
     }
