36fae9fe6eaf00dc480845f25c9fc2cdd391e9a3
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-05-14 10:03:09 -0500

    Add utils methods

--- Services/BulkSeedService.cs
+++ Services/BulkSeedService.cs
@@ -10,16 +10,44 @@ namespace BackendJPMAnalysis.Services
     {
         private readonly JPMDatabaseContext _context;
 
-        private readonly DataAccessService _dataAccessService;
+        private readonly ReportValidationsService _reportValidationsService;
 
 
         public BulkSeedService(
             JPMDatabaseContext context,
-            DataAccessService dataAccessService
+            ReportValidationsService reportValidationsService
         )
         {
             _context = context;
-            _dataAccessService = dataAccessService;
+            _reportValidationsService = reportValidationsService;
+        }
+
+
+        /// <summary>
+        /// The ClearDatabase method clears all data from multiple tables in a database while
+        /// temporarily disabling foreign key checks.
+        /// </summary>
+        public async Task ClearDatabase()
+        {
+            await _context.Database.ExecuteSqlRawAsync("SET FOREIGN_KEY_CHECKS = 0;");
+
+            try
+            {
+                _context.Accounts.RemoveRange(_context.Accounts);
+                _context.CompanyUsers.RemoveRange(_context.CompanyUsers);
+                _context.Functions.RemoveRange(_context.Functions);
+                _context.Profiles.RemoveRange(_context.Profiles);
+                _context.ProfilesFunctions.RemoveRange(_context.ProfilesFunctions);
+                _context.Products.RemoveRange(_context.Products);
+                _context.ProductsAccounts.RemoveRange(_context.ProductsAccounts);
+                _context.UserEntitlements.RemoveRange(_context.UserEntitlements);
+
+                await _context.SaveChangesAsync();
+            }
+            finally
+            {
+                await _context.Database.ExecuteSqlRawAsync("SET FOREIGN_KEY_CHECKS = 1;");
+            }
         }
 
 
@@ -30,14 +58,84 @@ namespace BackendJPMAnalysis.Services
         /// <param name="data">The ExcelDataDTO object containing seed data to post.</param>
         public async Task PostSeedInDatabase(ExcelDataDTO data)
         {
+            await ClearDatabase();
+
             await BulkPost(data.Accounts, "accounts");
             await BulkPost(data.Products, "products");
             await BulkPost(data.Functions, "functions");
             await BulkPost(data.Profiles, "profiles");
-            await BulkPost(data.CompanyUsers, "companyUsers", ValidateProfiles);
-            await BulkPost(data.ProductsAccounts, "productsAccounts", ValidateProductsAndAccounts);
-            await BulkPost(data.UsersEntitlements, "usersEntitlements", ValidateRelationsForUserEntitlement);
-            await BulkPost(data.ProfilesFunctions, "profiles_functions", ValidateProfilesAndFunctions);
+            await BulkPost(data.CompanyUsers, "companyUsers", _reportValidationsService.ValidateProfiles);
+            await BulkPost(data.ProductsAccounts, "productsAccounts", _reportValidationsService.ValidateProductsAndAccounts);
+            await BulkPost(data.UsersEntitlements, "usersEntitlements", _reportValidationsService.ValidateRelationsForUserEntitlement);
+            await BulkPost(data.ProfilesFunctions, "profiles_functions", _reportValidationsService.ValidateProfilesAndFunctions);
+        }
+
+
+        /// <summary>
+        /// Approves the changes specified in the provided data, including deletion, update, and addition of entities, and returns the total number of affected rows.
+        /// </summary>
+        /// <param name="data">The data containing entities to be deleted, updated, or added.</param>
+        /// <returns>The total number of affected rows after approving the changes.</returns>
+        public async Task<int> ApproveChanges(EntitiesToChangesDTO data)
+        {
+            int affectedRows = 0;
+
+            affectedRows += await BulkDelete<ProfileFunctionModel>(data.ProfilesFunctions.ToDeleteEntities);
+            affectedRows += await BulkDelete<UserEntitlementModel>(data.UsersEntitlements.ToDeleteEntities);
+            affectedRows += await BulkDelete<ProductAccountModel>(data.ProductsAccounts.ToDeleteEntities);
+            affectedRows += await BulkDelete<CompanyUserModel>(data.CompanyUsers.ToDeleteEntities);
+            affectedRows += await BulkDelete<ProfileModel>(data.Profiles.ToDeleteEntities);
+            affectedRows += await BulkDelete<FunctionModel>(data.Functions.ToDeleteEntities);
+            affectedRows += await BulkDelete<ProductModel>(data.Products.ToDeleteEntities);
+            affectedRows += await BulkDelete<AccountModel>(data.Accounts.ToDeleteEntities);
+
+
+            affectedRows += await BulkUpdate(data.Accounts.ToChangesEntities);
+            affectedRows += await BulkUpdate(data.Products.ToChangesEntities);
+            affectedRows += await BulkUpdate(data.Functions.ToChangesEntities);
+            affectedRows += await BulkUpdate(data.Profiles.ToChangesEntities);
+            affectedRows += await BulkUpdate(data.CompanyUsers.ToChangesEntities);
+            affectedRows += await BulkUpdate(data.ProductsAccounts.ToChangesEntities);
+            affectedRows += await BulkUpdate(data.UsersEntitlements.ToChangesEntities);
+            affectedRows += await BulkUpdate(data.ProfilesFunctions.ToChangesEntities);
+
+
+            affectedRows += await BulkPost(data.Accounts.NewEntities, "accounts");
+            affectedRows += await BulkPost(data.Products.NewEntities, "products");
+            affectedRows += await BulkPost(data.Functions.NewEntities, "functions");
+            affectedRows += await BulkPost(data.Profiles.NewEntities, "profiles");
+            affectedRows += await BulkPost(data.CompanyUsers.NewEntities, "companyUsers", _reportValidationsService.ValidateProfiles);
+            affectedRows += await BulkPost(data.ProductsAccounts.NewEntities, "productsAccounts", _reportValidationsService.ValidateProductsAndAccounts);
+            affectedRows += await BulkPost(data.UsersEntitlements.NewEntities, "usersEntitlements", _reportValidationsService.ValidateRelationsForUserEntitlement);
+            affectedRows += await BulkPost(data.ProfilesFunctions.NewEntities, "profiles_functions", _reportValidationsService.ValidateProfilesAndFunctions);
+
+            return affectedRows;
+        }
+
+
+        /// <summary>
+        /// Updates the report history in the database with the specified details.
+        /// </summary>
+        /// <param name="fileName">The name of the report file.</param>
+        /// <param name="appUserId">The ID of the user who uploaded the report (optional).</param>
+        /// <param name="observations">Observations or comments related to the report (optional).</param>
+        /// <param name="runReportDate">The date when the report was run (optional).</param>
+        public async Task UpdateReportHistory(string fileName, string? appUserId, string? observations, DateTime? runReportDate)
+        {
+            _context.ReportHistories.Add(
+                new ReportHistoryModel
+                {
+                    Id = fileName + DateTime.Now.ToString(),
+                    // AppUserId = appUserId,
+                    AppUserId = null,
+                    ReportName = fileName,
+                    ReportComments = (observations != "" && observations != null) ? observations : null,
+                    ReportUploadDate = DateTime.Now,
+                    RunReportDate = runReportDate
+                }
+            );
+
+            await _context.SaveChangesAsync();
         }
 
 
@@ -64,7 +162,7 @@ namespace BackendJPMAnalysis.Services
         /// <param name="entityName">The name of the entity being inserted.</param>
         /// <param name="validateDuplicates">An optional method to validate duplicate entities 
         /// within the collection.</param>
-        private async Task BulkPost<T>(
+        private async Task<int> BulkPost<T>(
             ICollection<T> collectionBody, string entityName,
             Func<ICollection<T>, Task<List<string>>>? validateDuplicates = null
         ) where T : class
@@ -78,117 +176,70 @@ namespace BackendJPMAnalysis.Services
                 }
             }
 
-            var duplicateEntries = await FindDuplicateEntities(collectionBody);
+            /* var duplicateEntries = await FindDuplicateEntities(collectionBody);
             if (duplicateEntries.Any())
             {
                 throw new BadRequestException($"Se encontraron {duplicateEntries.Count} registros duplicados en la base de datos para '{entityName}'. Estos registros ya existen y no pueden ser agregados.");
-            }
+            } */
 
             await _context.Set<T>().AddRangeAsync(collectionBody);
-            await _context.SaveChangesAsync();
+            int affectedRows = await _context.SaveChangesAsync();
+
+            return affectedRows;
         }
 
 
         /// <summary>
-        /// Asynchronously validates the profiles referenced in the collection of CompanyUserModel entities.
+        /// Deletes entities of type T from the database in bulk based on the provided primary keys (PKs) and returns the total number of affected rows.
         /// </summary>
-        /// <param name="collectionBody">The collection of CompanyUserModel entities to validate profiles for.</param>
-        /// <returns>A list of invalid profile IDs found in the collection.</returns>
-        private async Task<List<string>> ValidateProfiles(ICollection<CompanyUserModel> collectionBody)
+        /// <typeparam name="T">The type of entities to delete.</typeparam>
+        /// <param name="pks">The collection of primary keys (PKs) of entities to delete.</param>
+        /// <returns>The total number of affected rows after deleting the entities.</returns>
+        private async Task<int> BulkDelete<T>(ICollection<string> pks) where T : class
         {
-            var profilesIds = collectionBody.Select(pf => pf.ProfileId).Distinct().ToList();
+            var pksList = pks.ToList();
 
-            var existingProfiles = await _dataAccessService.CheckExistingEntitiesAsync<ProfileModel>(profilesIds);
+            var entities = await _context.Set<T>().ToListAsync();
 
-            var invalidProfilesIds = profilesIds.Where(id => !existingProfiles.Contains(id)).ToList();
+            var entitiesToDelete = entities
+                .Where(entity => pksList.Contains(entity.GetId()))
+                .ToList();
 
-            return invalidProfilesIds.Distinct().ToList();
+            var nonExistentPks = pksList.Except(entitiesToDelete.Select(pf => pf.GetId()));
+            if (nonExistentPks.Any())
+            {
+                throw new BadRequestException($"Los siguientes PKs no existen en la base de datos: {string.Join(", ", nonExistentPks)}");
+            }
+
+            _context.Set<T>().RemoveRange(entitiesToDelete);
+            int affectedRows = await _context.SaveChangesAsync();
+
+            return affectedRows;
         }
 
 
         /// <summary>
-        /// Asynchronously validates the products and accounts referenced in the collection of 
-        /// ProductAccountModel entities.
+        /// Updates entities of type T in bulk in the database based on the provided collection of entities and returns the total number of affected rows.
         /// </summary>
-        /// <param name="collectionBody">The collection of ProductAccountModel entities to validate 
-        /// products and accounts for.</param>
-        /// <returns>A list of invalid product IDs and account numbers found in the collection.</returns>
-        private async Task<List<string>> ValidateProductsAndAccounts(ICollection<ProductAccountModel> collectionBody)
+        /// <typeparam name="T">The type of entities to update.</typeparam>
+        /// <param name="collectionBody">The collection of entities containing the updated data.</param>
+        /// <returns>The total number of affected rows after updating the entities.</returns>
+        private async Task<int> BulkUpdate<T>(ICollection<T> collectionBody) where T : class
         {
-            var productsIds = collectionBody
-                .Where(p => p.ProductId != null)
-                .Select(p => p.ProductId).Distinct().ToList();
-            var accountsNumber = collectionBody
-                .Where(a => a.AccountNumber != null)
-                .Select(a => a.AccountNumber).Distinct().ToList();
+            foreach (var updatedEntity in collectionBody)
+            {
+                var pk = updatedEntity.GetId();
+                var existingEntity = await _context.Set<T>().FindAsync(pk);
 
-            var existingProducts = await _dataAccessService.CheckExistingEntitiesAsync<ProductModel>(productsIds!);
-            var existingAccounts = await _dataAccessService.CheckExistingEntitiesAsync<AccountModel>(accountsNumber!);
+                if (existingEntity != null)
+                {
+                    _context.Entry(existingEntity).CurrentValues.SetValues(updatedEntity);
+                }
+            }
 
-            var invalidProductsIds = productsIds.Where(id => !existingProducts.Contains(id!)).ToList();
-            var invalidAccountsNumbers = accountsNumber.Where(id => !existingAccounts.Contains(id!)).ToList();
+            int affectedRows = await _context.SaveChangesAsync();
 
-            return invalidProductsIds.Concat(invalidAccountsNumbers).Distinct().ToList()!;
-        }
-
-
-        /// <summary>
-        /// Asynchronously validates the relations (company users, accounts, products, and functions) 
-        /// referenced in the collection of UserEntitlementModel entities.
-        /// </summary>
-        /// <param name="collectionBody">The collection of UserEntitlementModel entities to validate 
-        /// relations for.</param>
-        /// <returns>A list of invalid IDs found in the collection.</returns>
-        private async Task<List<string>> ValidateRelationsForUserEntitlement(ICollection<UserEntitlementModel> collectionBody)
-        {
-            var companyUsersIds = collectionBody.Select(ue => ue.AccessId).Distinct().ToList();
-            var accountsNumber = collectionBody
-                .Where(ue => ue.AccountNumber != null)
-                .Select(ue => ue.AccountNumber).Distinct().ToList();
-            var productsIds = collectionBody
-                .Where(ue => ue.ProductId != null)
-                .Select(ue => ue.ProductId).Distinct().ToList();
-            var functionsIds = collectionBody
-                .Where(ue => ue.FunctionId != null)
-                .Select(ue => ue.FunctionId).Distinct().ToList();
-
-            var existingCompanyUsers = await _dataAccessService.CheckExistingEntitiesAsync<CompanyUserModel>(companyUsersIds);
-            var existingAccounts = await _dataAccessService.CheckExistingEntitiesAsync<AccountModel>(accountsNumber!);
-            var existingProducts = await _dataAccessService.CheckExistingEntitiesAsync<ProductModel>(productsIds!);
-            var existingFunctions = await _dataAccessService.CheckExistingEntitiesAsync<FunctionModel>(functionsIds!);
-
-            var invalidCompanyUsers = companyUsersIds.Where(id => !existingCompanyUsers.Contains(id!)).ToList();
-            var invalidAccountsNumbers = accountsNumber.Where(id => !existingAccounts.Contains(id!)).ToList();
-            var invalidProductsIds = productsIds.Where(id => !existingProducts.Contains(id!)).ToList();
-            var invalidFunctionsIds = functionsIds.Where(id => !existingFunctions.Contains(id!)).ToList();
-
-            return invalidCompanyUsers
-                .Concat(invalidAccountsNumbers)
-                .Concat(invalidProductsIds)
-                .Concat(invalidFunctionsIds)
-                .Distinct().ToList()!;
-        }
-
-
-        /// <summary>
-        /// Asynchronously validates the profiles and functions referenced in the collection of 
-        /// ProfileFunctionModel entities.
-        /// </summary>
-        /// <param name="collectionBody">The collection of ProfileFunctionModel entities to validate 
-        /// profiles and functions for.</param>
-        /// <returns>A list of invalid profile and function IDs found in the collection.</returns>
-        private async Task<List<string>> ValidateProfilesAndFunctions(ICollection<ProfileFunctionModel> collectionBody)
-        {
-            var profilesIds = collectionBody.Select(pf => pf.ProfileId).Distinct().ToList();
-            var functionsIds = collectionBody.Select(pf => pf.FunctionId).Distinct().ToList();
-
-            var existingProfiles = await _dataAccessService.CheckExistingEntitiesAsync<ProfileModel>(profilesIds);
-            var existingFunctions = await _dataAccessService.CheckExistingEntitiesAsync<FunctionModel>(functionsIds);
-
-            var invalidProfilesIds = profilesIds.Where(id => !existingProfiles.Contains(id)).ToList();
-            var invalidFunctionsIds = functionsIds.Where(id => !existingFunctions.Contains(id)).ToList();
-
-            return invalidProfilesIds.Concat(invalidFunctionsIds).Distinct().ToList();
+            return affectedRows;
         }
     }
 }
