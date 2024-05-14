36b7aee00f65a4996ace2926d15fcbe37b4f4db6
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-05-14 11:34:43 -0500

    Update Data

--- DOC/sql/profiles.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO profiles (id, profile_name) VALUES ('capture', 'Capture');
-INSERT INTO profiles (id, profile_name) VALUES ('verification', 'Verification');
-INSERT INTO profiles (id, profile_name) VALUES ('authorization', 'Authorization');
-INSERT INTO profiles (id, profile_name) VALUES ('administrator', 'Administrador');
-INSERT INTO profiles (id, profile_name) VALUES ('viewer', 'Viewer');
