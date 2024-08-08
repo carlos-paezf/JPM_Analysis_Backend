5e4bbcb582c8314a1a6fe70cb7a5834229c3946f
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-08-08 17:15:11 -0500

    Add User Circularization DTO

--- DTO/UserCircularizationDTO.cs
+++ DTO/UserCircularizationDTO.cs
@@ -1,20 +1,33 @@
-using System.ComponentModel.DataAnnotations;
+using BackendJPMAnalysis.Models;
 
 
 namespace BackendJPMAnalysis.DTO
 {
-    public class UserDepartmentAssignmentDTO
+    public class UserCircularizationDTO
     {
-        [Required(ErrorMessage = "La propiedad accessId es requerida")]
+        public string Name { get; set; }
+
         public string AccessId { get; set; }
 
-        [Required(ErrorMessage = "La propiedad departmentInitials es requerida")]
         public string DepartmentInitials { get; set; }
 
-        public UserDepartmentAssignmentDTO(string accessId, string departmentInitials)
+        public string DepartmentName { get; set; }
+
+        public string ProfileName { get; set; }
+
+        public bool Status { get; set; }
+
+        public string Email { get; set; }
+
+        public UserCircularizationDTO(CompanyUserModel companyUser, DepartmentModel? department, ProfileModel profile)
         {
-            AccessId = accessId;
-            DepartmentInitials = departmentInitials;
+            Name = companyUser.UserName;
+            AccessId = companyUser.AccessId;
+            DepartmentInitials = department?.Initials ?? "No Department";
+            DepartmentName = department?.DepartmentName ?? "No Department";
+            ProfileName = profile.ProfileName;
+            Status = companyUser.UserStatus;
+            Email = companyUser.EmailAddress;
         }
     }
 }
